6a006f5982fef2c42f8b765568e07b8f7987726a
Héctor <hector951357q@hotmail.com>
2022-10-24 12:22:33 +0200

    cambios dia 3

---
+++ day_1/helloworld.py
@@ -0,0 +1,36 @@
+1.2
+print(3 + 4)
+print(3 -4)
+print(3 * 4)
+print(3%4)
+print(3 / 4)
+print(3**4)
+print(3//4)
+
+1.3
+print("hector")
+print("mayta")
+print("spain")
+print("i'm enjoying 30 days of python")
+
+1.4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "python", "findland"]))
+print(type("hector"))
+print(type("mayta"))
+print(type("spain"))
+
+3.1
+print("integer is 2, float is 2.2, complex is 2 - 3i" )
+print("string is 'hola'")
+print("list is ['puerta', 'abrir']")
+print("boolean is 'true'")
+
+3.2
+import math
+a = 2
+b = 3
+print(math.dist(a,b))
