f8c0a5552f75832b3c290fb68a48f1a417908563
Héctor <hector951357q@hotmail.com>
2022-10-25 13:26:53 +0200

    cambios dia 25

---
+++ day2/variables.py
@@ -0,0 +1,71 @@
+1
+1.2
+#Day2: 30 days of python programming
+1.3
+First_name = "hector"
+1.4
+Last_name = "mayta"
+1.5
+Full_name = "hector sebastian mayta duran"
+1.6
+Country = "spain"
+1.7
+City = "jerez"
+1.8
+Age = "17"
+1.9
+Year = "2022"
+1.10
+Is_married = "no"
+1.11
+Is_true = "yes"
+1.12
+Is_light_on = "no"
+1.13
+n1, n2, n3 = 4, 1, 3
+
+2
+2.1
+print(type(First_name))
+print(type(Last_name))
+print(type(Full_name))
+print(type(Country))
+print(type(City))
+print(type(Age))
+print(type(Year))
+print(type(Is_married))
+print(type(Is_true))
+print(type(Is_light_on))
+
+2.2
+print(len(First_name))
+
+2.3
+print(len(First_name))
+print(len(Last_name))
+
+2.4
+num_one = 5
+num_two = 4
+
+2.41
+total = num_one + num_two 
+
+2.42
+diff = num_one - num_two
+
+2.43
+product = num_two * num_one
+
+2.44
+division = num_one / num_two
+
+2.45
+remainder = num_two / num_one 
+
+2.46
+exp = num_one ** num_two 
+
+2.47
+floor_division = num_one // num_two
+print(floor_division)
