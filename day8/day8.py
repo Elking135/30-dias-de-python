2ce7a93e13971eed95374080e1e181888ad0a992
pabloarmarioandrades <pabloarmarioandrades14@gmail.com>
2023-02-13 12:04:58 +0100

    day8 y 9

---
+++ day8/day8.py
@@ -0,0 +1,28 @@
+1
+dog = {}
+
+2
+dog["color"] = "brown"
+dog["age"] = 6
+dog["legs"] = 4
+dog["breed"] = "chihuahua"
+
+3
+student = {"first_name":"Hector", "last_name":"Mayta", "gender":"male", "age": 17, "marital_estatus": "bachelor", "skills": "nothing", "country":"spain", "city":"jerez de la frontera", "adress":"11406"}
+
+4
+len(student)
+
+5
+print(type('skills' in student))
+
+6
+student['skills'] = 'study and sleep'
+
+7
+keys = student.keys()
+
+8
+values = student.values()
+
+9
